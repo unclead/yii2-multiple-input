444d22b7e96e106e586fb1b8e090cddfec6e4663
Carsten Brandt <mail@cebe.cc>
2021-10-01 10:00:42 +0200

    prevent error loop in case of undefined `$wrapper.data('multipleInput')`
    if for any reason this code line fails, the interval is not removed and
    console.log fills with error messages every 10ms.
    
    This change prevents that from happening.
    
    I have no idea why it fails, but the widget works fine.

--- src/assets/src/js/jquery.multipleInput.js
+++ src/assets/src/js/jquery.multipleInput.js
@@ -212,10 +212,13 @@
                 // If after a second system could not detect ActiveForm it means
                 // that widget is used without ActiveForm and we should just complete initialization of the widget
                 if (form.length === 0 || i > 10) {
-                    $wrapper.data('multipleInput').currentIndex = findMaxRowIndex($wrapper);
+                    clearInterval(intervalID);
                     isActiveFormEnabled = false;
 
-                    clearInterval(intervalID);
+                    if (typeof $wrapper.data('multipleInput') !== 'undefined') {
+                        $wrapper.data('multipleInput').currentIndex = findMaxRowIndex($wrapper);
+                    }
+
                     $wrapper.trigger(event);
                 }
             }, 100);
